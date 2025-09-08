98b2552200fa342f98470cb76168562ff135ac61
Simple002 <meyrzhan04.04@mail.ru>
2025-09-08 17:53:44 +0500

    first

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import { useEffect, useState } from "react";
+
+function App() {
+  const [message, setMessage] = useState("");
+
+  useEffect(() => {
+    fetch(`${import.meta.env.VITE_API_URL}/api/hello`) // запрос на backend
+      .then((res) => res.json())
+      .then((data) => setMessage(data.message));
+  }, []);
+
+  return (
+    <div style={{ textAlign: "center", marginTop: "50px" }}>
+      <h1>React + Vite + Express</h1>
+      <p>{message}</p>
+    </div>
+  );
+}
+
+export default App;
